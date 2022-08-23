6ad3c84fad0bb24614c1ad0ccd0c530090962763
Ahlul Esasjana <cakahlul@gmail.com>
2022-08-23 18:49:01 +0700

    add simple assertion from http res

--- cypress/e2e/heroes.cy.ts
+++ cypress/e2e/heroes.cy.ts
@@ -1,6 +1,27 @@
 describe('Heroes list', () => {
   it('should show heading My Heroes when open heroes page', () => {
     cy.visit('localhost:4200/heroes');
+
     cy.get('h2').should('contain', 'My Heroes');
+  });
+
+  it('should fetch list heroes from api/heroes url', () => {
+    const heroList = [
+      { id: 1, name: 'Dr Nice' },
+      { id: 2, name: 'Narco' },
+    ];
+    cy.intercept(
+      {
+        method: 'GET',
+        url: 'api/heroes',
+      },
+      heroList,
+    ).as('getHeroes');
+
+    cy.visit('localhost:4200/heroes');
+
+    cy.get('[data-cy="heroes"]')
+      .should('contain', 'Dr Nice')
+      .and('contain', 'Narco');
   });
 });
