f6898d7d35e0d04b56b3efaac54fb7dd221ba971
Ahlul Esasjana <cakahlul@gmail.com>
2022-08-24 09:25:25 +0700

    assert response API

--- cypress/e2e/heroes.cy.ts
+++ cypress/e2e/heroes.cy.ts
@@ -1,11 +1,16 @@
 describe('Heroes list', () => {
+  const heroList = [
+    { id: 1, name: 'Dr Nice' },
+    { id: 2, name: 'Narco' },
+  ];
+
   it('should show heading My Heroes when open heroes page', () => {
     cy.visit('localhost:4200/heroes');
 
     cy.get('h2').should('contain', 'My Heroes');
   });
 
-  it('should fetch list heroes from api/heroes url', () => {
+  it('should return response of heroList when fetch from api/heroes url', () => {
     const heroList = [
       { id: 1, name: 'Dr Nice' },
       { id: 2, name: 'Narco' },
@@ -20,8 +25,25 @@ describe('Heroes list', () => {
 
     cy.visit('localhost:4200/heroes');
 
-    cy.get('[data-cy="heroes"]')
-      .should('contain', 'Dr Nice')
-      .and('contain', 'Narco');
+    cy.wait('@getHeroes').then(interception => {
+      assert.deepEqual(interception.response.body, heroList);
+    });
+  });
+
+  it('should show list heroes when fetch data from api/heroes url', () => {
+    cy.intercept(
+      {
+        method: 'GET',
+        url: 'api/heroes',
+      },
+      heroList,
+    ).as('getHeroes');
+
+    cy.visit('localhost:4200/heroes');
+    cy.wait('@getHeroes').then(() => {
+      cy.get('[data-cy="heroes"]')
+        .should('contain', 'Dr Nice')
+        .and('contain', 'Narco');
+    });
   });
 });
